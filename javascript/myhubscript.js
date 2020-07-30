9b0a34744bd6a8af2f7f551fa3cfb7b867524355
shubham0809200 <shubham.kr08092000@gmail.com>
2020-07-30 08:25:34 +0530

    made up th menu

---
+++ javascript/myhubscript.js
@@ -0,0 +1,42 @@
+function firstt()
+{
+    document.getElementById('firstSheet').style.display='block';
+    document.getElementById('secondSheet').style.display='none';
+    document.getElementById('thirdSheet').style.display='none';
+    document.getElementById('fourthSheet').style.display='none';
+
+
+}
+
+function secondd()
+{
+    document.getElementById('secondSheet').style.display='block';
+    document.getElementById('firstSheet').style.display='none';
+    document.getElementById('thirdSheet').style.display='none';
+    document.getElementById('fourthSheet').style.display='none';
+
+
+
+}
+
+function thirdd()
+{
+    document.getElementById('thirdSheet').style.display='block';
+    document.getElementById('firstSheet').style.display='none';
+    document.getElementById('secondSheet').style.display='none';
+    document.getElementById('fourthSheet').style.display='none';
+
+
+}
+
+function fourthh()
+{
+    document.getElementById('fourthSheet').style.display='block';
+    document.getElementById('firstSheet').style.display='none';
+    document.getElementById('secondSheet').style.display='none';
+    document.getElementById('thirdSheet').style.display='none';
+    
+
+}
+
+
