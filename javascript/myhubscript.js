42d01168912b65f8ed0f72a878a1e04b072c0382
shubham0809200 <shubham.kr08092000@gmail.com>
2020-08-04 18:14:43 +0530

    almost done

--- javascript/myhubscript.js
+++ javascript/myhubscript.js
@@ -39,4 +39,77 @@ function fourthh()
 
 }
 
+var c=0;
+
+
+function hotel(){
+
+            if(c===1)
+            {
+                document.getElementById('hotell').style.display='none';
+                c=0;
+            }
+            else
+            {
+                document.getElementById('hotell').style.display='block';
+                c=1;
+            }
+
+
+}
+
+
+var s=0;
+function shops(){
+
+    if(s===1)
+    {
+        document.getElementById('shopp').style.display='none';
+        s=0;
+    }
+    else
+    {
+        document.getElementById('shopp').style.display='block';
+        s=1;
+    }
+
+
+}
+
+
+var o=0;
+function office(){
+
+    if(o===1)
+    {
+        document.getElementById('officee').style.display='none';
+        o=0;
+    }
+    else
+    {
+        document.getElementById('officee').style.display='block';
+        o=1;
+    }
+
+
+}
+
+
+var ot=0;
+function other(){
+
+    if(ot===1)
+    {
+        document.getElementById('otherr').style.display='none';
+        ot=0;
+    }
+    else
+    {
+        document.getElementById('otherr').style.display='block';
+        ot=1;
+    }
+
+
+}
+
 
